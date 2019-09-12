699d576def961ff90db5e7d00dd0853645d3175f
knadeem1 <35601623+knadeem1@users.noreply.github.com>
2019-09-12 15:28:50 -0400

    initialized

---
+++ app/src/main/java/edu/binghamton/cs/cs441_project_2/MySurface.java
@@ -0,0 +1,18 @@
+package edu.binghamton.cs.cs441_project_2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.PixelFormat;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+public class MySurface extends SurfaceView implements SurfaceHolder.Callback {
+    constructing a unit circle pie with different portion pieces that the user can choose from.
+    -each size portion will have a different color
+    -user will be able to pick what size and drag and drop the shape in the desired place
+    -bottom of the screen will show how much has been added and how much space is left (what pieces can still be added)
+        maybe the pieces that cant fit will fade away or dissapear
+}
