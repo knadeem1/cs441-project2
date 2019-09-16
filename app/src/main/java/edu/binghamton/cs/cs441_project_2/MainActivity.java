719461ba428337e40f465ab15924771e5d96763c
knadeem1 <35601623+knadeem1@users.noreply.github.com>
2019-09-16 16:25:37 -0400

    working model

--- app/src/main/java/edu/binghamton/cs/cs441_project_2/MainActivity.java
+++ app/src/main/java/edu/binghamton/cs/cs441_project_2/MainActivity.java
@@ -2,14 +2,166 @@ package edu.binghamton.cs.cs441_project_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    private Button showGrid = null;
+    private Button button1 = null;
+    private Button button2 = null;
+    private Button button3 = null;
+    private Button button4 = null;
+    private LinearLayout canvasLayout = null;
+    MySurface customView = null;
+    private boolean drawPiece1 = false;
+    private boolean drawPiece2 = false;
+    private boolean drawPiece3 = false;
+    private boolean drawPiece4 = false;
+    private boolean drawGrid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("SurfaceView");
+
+        initControls();
+
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        customView = new MySurface(getApplicationContext());
+        customView.setOnTouchListener(this);
+        canvasLayout.addView(customView);
+
+        showGrid.setVisibility(View.VISIBLE);
+        button1.setVisibility(View.GONE);
+        button2.setVisibility(View.GONE);
+        button3.setVisibility(View.GONE);
+        button4.setVisibility(View.GONE);
+
+        // Click this button to draw a red circle ball move after finger touch.
+        showGrid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawGrid = true;
+                button1.setVisibility(View.VISIBLE);
+                button2.setVisibility(View.VISIBLE);
+                button3.setVisibility(View.VISIBLE);
+                button4.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // Click this button to draw a red circle ball move after finger touch.
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawPiece1 = true;
+            }
+        });
+
+        // Click this button to draw a green rectangle move after finger touch.
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawPiece2 = true;
+            }
+        });
+
+        // Click this button to draw a green rectangle move after finger touch.
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawPiece3 = true;
+            }
+        });
+
+        // Click this button to draw a green rectangle move after finger touch.
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawPiece4 = true;
+            }
+        });
     }
+    private void initControls(){
+        if(canvasLayout == null){
+            canvasLayout = findViewById(R.id.customViewLayout);
+        }
+        if(showGrid == null){
+            showGrid = findViewById(R.id.showGrid);
+        }
+        if(button1 == null){
+            button1 = findViewById(R.id.Button1);
+        }
+        if(button2 == null){
+            button2 = findViewById(R.id.Button2);
+        }
+        if(button3 == null){
+            button3 = findViewById(R.id.Button3);
+        }
+        if(button4 == null){
+            button4 = findViewById(R.id.Button4);
+        }
+
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent){
+        // If user touch the custom SurfaceView object.
+        if(view instanceof SurfaceView) {
+            view.invalidate();
+
+            float x = motionEvent.getX();
+            float y = motionEvent.getY();
+            customView.setPointerX(x);
+            customView.setPointerY(y);
+
+            if (drawGrid) {
+                Paint paint = new Paint();
+                paint.setColor(Color.BLACK);
+                customView.setPaint(paint);
+                customView.drawGrid();
+            }
+            if (drawPiece1) {
+                Paint paint = new Paint();
+                paint.setColor(Color.GREEN);
+                customView.setPaint(paint);
+                customView.drawPiece(1);
+            }
+            if (drawPiece2) {
+                Paint paint = new Paint();
+                paint.setColor(Color.YELLOW);
+                customView.setPaint(paint);
+                customView.drawPiece(2);
+            }
+            if (drawPiece3) {
+                Paint paint = new Paint();
+                paint.setColor(Color.BLUE);
+                customView.setPaint(paint);
+                customView.drawPiece(3);
+            }
+            if (drawPiece4) {
+                Paint paint = new Paint();
+                paint.setColor(Color.RED);
+                customView.setPaint(paint);
+                customView.drawPiece(4);
+            }
+
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
 }
--- app/src/main/java/edu/binghamton/cs/cs441_project_2/MySurface.java
+++ app/src/main/java/edu/binghamton/cs/cs441_project_2/MySurface.java
@@ -1,18 +1,133 @@
 package edu.binghamton.cs.cs441_project_2;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.PixelFormat;
+import android.graphics.RectF;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
 
 public class MySurface extends SurfaceView implements SurfaceHolder.Callback {
-    constructing a unit circle pie with different portion pieces that the user can choose from.
-    -each size portion will have a different color
-    -user will be able to pick what size and drag and drop the shape in the desired place
-    -bottom of the screen will show how much has been added and how much space is left (what pieces can still be added)
-        maybe the pieces that cant fit will fade away or dissapear
+    private SurfaceHolder surfaceHolder = null;
+    private Paint paint = null;
+    private RectF rectf = null;
+
+    private int numRows = 20;
+    private int numColumns = 20;
+
+    private float pointerX = 0;
+    private float pointerY = 0;
+
+
+
+    public MySurface(Context context) {
+        super(context);
+
+        surfaceHolder = getHolder();
+
+        paint = new Paint();
+        paint.setColor(Color.RED);
+
+        rectf = new RectF(getPointerX(), getPointerY(), 200, 200);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder){
+        drawGrid();
+    }
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2){
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder){
+        paint = null;
+    }
+
+    public void setPaint(Paint paint){
+        this.paint = paint;
+    }
+
+    public int getNumberOfRows(){
+        return numRows;
+    }
+
+    public int getNumberofColumns(){
+        return numColumns;
+    }
+
+    protected void drawGrid(){
+        Canvas canvas = surfaceHolder.lockCanvas();
+        Paint surfaceBackground = new Paint();
+        surfaceBackground.setColor(Color.WHITE);
+        canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), surfaceBackground);
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(15);
+
+        canvas.drawLine(width/2, 0, width/2, height, paint);
+        canvas.drawLine(0, height/2, width, height/2, paint);
+
+        paint.setStrokeWidth(5);
+
+        //columns
+        for (int i = 1; i <= getNumberofColumns(); i++) {
+            canvas.drawLine(width * i / numColumns, 0, width * i / numColumns, height, paint);
+        }
+
+        //rows
+        for (int i = 1; i <= getNumberOfRows(); i++) {
+            canvas.drawLine(0, height * i / numRows, width, height * i / numRows, paint);
+        }
+
+
+        surfaceHolder.unlockCanvasAndPost(canvas);
+    }
+
+
+    public float getPointerX(){
+        return pointerX;
+    }
+
+    public float getPointerY(){
+        return pointerY;
+    }
+
+    public void setPointerX(float pointerX) {
+        this.pointerX = pointerX;
+    }
+
+    public void setPointerY(float pointerY) {
+        this.pointerY = pointerY;
+    }
+
+    public void drawPiece(int option){
+        Canvas canvas = surfaceHolder.lockCanvas();
+        //Paint surfaceBackground = new Paint();
+        //surfaceBackground.setColor(Color.WHITE);
+        //canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), surfaceBackground);
+
+        // Draw the pie slices
+        if(option == 1) {
+            canvas.drawArc(rectf, 0F, 20F, true, paint);
+        }
+        if(option == 2) {
+            canvas.drawArc(rectf, 0F, 30F, true, paint);
+        }
+        if(option == 3) {
+            canvas.drawArc(rectf, 0F, 90F, true, paint);
+        }
+        if(option == 4) {
+            canvas.drawArc(rectf, 0F, 60F, true, paint);
+        }
+
+        surfaceHolder.unlockCanvasAndPost(canvas);
+    }
+
 }
